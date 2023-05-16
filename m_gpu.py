8a55d81561c7d477dd8aaebdaed0623e44d6efbb
Ziad <ziad-ahmed-abdelmotagaly.mohamed@inria.fr>
2023-05-16 15:53:01 +0200

    iterations

--- m_gpu.py
+++ m_gpu.py
@@ -5,8 +5,7 @@ import nipype.pipeline.engine as pe
 import nipype.interfaces.fsl as fsl
 import os
 import nipype.interfaces.utility as util
-from nipype import DataGrabber, DataSink
-from nipype.interfaces.utility import Merge
+from nipype import DataGrabber, DataSink, IdentityInterface
 from typing import List
 import numpy 
 from nilearn import plotting
@@ -15,6 +14,23 @@ from nipype.interfaces.utility.wrappers import Function
 import nipype.interfaces.ants as ants
 from nipype.interfaces.ants.base import ANTSCommand
 from nipype.interfaces import fsl
+import configparser
+import sys
+import nipype.interfaces.fsl.utils as fslu
+from nipype.interfaces.fsl.utils import Merge
+from nipype import config, logging
+
+
+config.update_config({'logging': {'log_directory': os.path.join(os.getcwd(), 'logs'),
+                                  'workflow_level': 'DEBUG',
+                                  'interface_level': 'DEBUG',
+                                  'log_to_file': True
+                                  },
+                      'execution': {'stop_on_first_crash': True},
+                    })
+
+
+
 
 
 # Define the paths to the input and output files
@@ -22,19 +38,45 @@ from nipype.interfaces import fsl
 out_dir = '/data/parietal/store/work/zmohamed/mathfun/output'
 PATH = '/data/parietal/store/work/zmohamed/mathfun/'
 
+
 # Create a Nipype workflow
 
 tractography_wf = pe.Workflow(name='tractography_wf',  base_dir=PATH)
 
+
+config = configparser.ConfigParser()
+config.read(sys.argv[1])
+
+
+subject_list = config['DEFAULT']['id_list'].split(" ")
+visits = list(config['DEFAULT']['visits'])
+subjects_dir = config['DEFAULT']['subjects_dir']
+sessions = list(config['DEFAULT']['sessions'])
+
+infosource = pe.Node(IdentityInterface(fields=['subject_id', 'visit','session']),
+                      name='subjects')
+infosource.iterables = [('subject_id', subject_list), ('visit', visits), ('session', sessions)]
+
+subject_id_visit = pe.Node(
+        interface=Function(
+            input_names=['subject_id', 'visit'], output_names=['composite_id'],
+            function=lambda subject_id, visit: '{}_{}'.format(subject_id, visit)
+        ),
+        name='subject_id_visit'
+)  
+
 data_source = pe.Node(DataGrabber(infields=[],
-                                   outfields=['dwi', 'bval', 'bvec', 'mask', 'roi','template', 'T1', 'T1_brain']),
+                                   outfields=['dwi', 'bval', 'bvec', 'mask', 'roi','template', 'T1', 'T1_brain', 'parc']),
                        name='input_node')
 
 data_source.inputs.sort_filelist = True
-data_source.inputs.base_directory = '/data/parietal/store/work/zmohamed/mathfun/'
+data_source.inputs.base_directory = '/data/parietal/store/work/zmohamed/mathfun/tp2'
 data_source.inputs.template = ''
+
+'''
 data_source.inputs.field_template = {
         'mask': '/data/parietal/store/work/zmohamed/mathfun/hcp_templates/MNI152_T1_1mm_brain_mask.nii.gz' ,
+        'parc': '/data/parietal/store/work/zmohamed/mathfun/dwi_rois/combined_BN_L_Hipp_roi.nii',  
         'roi':'/data/parietal/store/work/zmohamed/mathfun/dwi_rois/1mm/L_PPC_6mm_-44_-42_52_1mmiso_roi.nii.gz',
         'dwi': '/data/parietal/store/work/zmohamed/mathfun/tp2/7014/visit2/session1/dwi/dwi_raw.nii.gz',
         'bval': '/data/parietal/store/work/zmohamed/mathfun/tp2/7014/visit2/session1/dwi/dti_raw.bvals',
@@ -43,8 +85,121 @@ data_source.inputs.field_template = {
         'T1_brain': '/data/parietal/store/work/zmohamed/mathfun/hcp_templates/MNI152_T1_1mm_brain.nii.gz',
     
     }
+'''
+data_source.inputs.field_template = {
+        'T1': '%s/visit%s/session%s/anat/T1w.nii',
+        'dwi': '%s/visit%s/session%s/dwi/dwi_raw.nii.gz',
+        'bval': '%s/visit%s/session%s/dwi/dti_raw.bvals',
+        'bvec': '%s/visit%s/session%s/dwi/dti_raw.bvecs',
+        
+        #'bval': '%s/visit%s/session%s/dwi/dwepi.150.bvals',
+        #'bvec': '%s/visit%s/session%s/dwi/dwepi.150.grads'
+    }
+data_source.inputs.template_args = {
+        template: [['subject_id', 'visit','session']]
+        for template in data_source.inputs.field_template.keys()
+    }
 
-    # Where can i find the MNI ? 
+tractography_wf.connect(infosource, 'subject_id', data_source, 'subject_id')
+tractography_wf.connect(infosource, 'visit', data_source, 'visit')
+tractography_wf.connect(infosource, 'session', data_source, 'session')
+
+
+roi_source = pe.Node(DataGrabber(infields=[]),
+                           name='rois')
+roi_source.inputs.sort_filelist = True
+roi_source.inputs.base_directory = '/data/parietal/store/work/zmohamed/mathfun/dwi_rois/1mm/'
+roi_source.inputs.template = '*.nii.gz'
+
+
+template_source = pe.Node(DataGrabber(infields=[], outfields=['T1', 'T1_brain', 'T1_mask', 'T2', 'T2_brain', 'T2_mask']),
+                           name='mni_template')
+template_source.inputs.sort_filelist = True
+template_source.inputs.base_directory = config['TEMPLATE']['directory']
+template_source.inputs.template = ''
+template_source.inputs.field_template = {
+        'T1': config['TEMPLATE']['T1'],
+        'T1_brain': config['TEMPLATE']['T1_brain'],
+        'T1_mask': config['TEMPLATE']['T1_mask'],
+        'T2': config['TEMPLATE']['T2'],
+        'T2_brain': config['TEMPLATE']['T2_brain'],
+    }
+template_source.inputs.template_args = {
+        template: []
+        for template in template_source.inputs.field_template.keys()
+    }
+
+
+# Create a Node for the Merge interface
+merge = pe.Node(interface=fslu.Merge(dimension='t',
+                                      output_type='NIFTI_GZ'),
+                name='merge')
+
+# Set the input files to be merged and the output file name
+merge.inputs.merged_file = '/data/parietal/store/work/zmohamed/mathfun/parcellations/merged_roi.nii.gz'
+
+tractography_wf.connect(roi_source, 'outfiles', merge, 'in_files')
+
+
+
+def mergeROI (roi_paths):
+    import nibabel as nib
+    from nilearn import plotting 
+    import os, os.path
+  
+    # Load the ROI images using nibabel
+    roi_images = [nib.load(roi_path) for roi_path in roi_paths]
+    merged_roi = nib.concat_images(roi_images, check_affines=True)
+
+# Set the affine and header information for the merged NIfTI file using the first ROI file's affine and header
+
+    nib.save(merged_roi, os.path.join(os.getcwd(),'parcel_image.nii.gz')) 
+    return os.path.join(os.getcwd(),'parcel_image.nii.gz')
+
+
+
+MergeROI = Function(
+    input_names=['roi_paths'], output_names=['output_image'],
+    function=mergeROI
+  )
+
+merge_ROI =  pe.Node(interface = MergeROI,
+          name = 'merge_ROI')
+
+
+tractography_wf.connect(roi_source, 'outfiles', merge_ROI, 'roi_paths')
+
+
+
+
+
+
+
+def plotNiiGZ (NIIGZ_file, background):
+    import nibabel as nib
+    from nilearn import plotting 
+    import os, os.path
+    output_file = os.path.join(
+        os.getcwd(),
+        f'{os.path.basename(NIIGZ_file)}.png'
+    )
+    niigz_img = nib.load(NIIGZ_file)
+    
+    plotting.plot_roi(niigz_img,bg_img = background , display_mode='mosaic', cut_coords= 5, title='NIIGZ map', output_file= output_file)
+    return output_file   
+
+PlotNiiGZ = Function(
+    input_names=['NIIGZ_file', 'background'], output_names=['output_image'],
+    function=plotNiiGZ
+  )
+
+plot_NiiGZ =  pe.Node(interface = PlotNiiGZ,
+          name = 'plot_NiiGZ')
+
+
+
+
+
 
 
 
@@ -58,8 +213,6 @@ mrconvert_nifti_to_mif = pe.Node(
 
 
 tractography_wf.connect(data_source, 'dwi', mrconvert_nifti_to_mif, 'in_file')
-
-
 
 
 dwiextract = pe.Node(
@@ -92,8 +245,6 @@ mrconvert_mif_to_nifti_b0 = pe.Node(
     ),
     name='mrconvert_mif_to_nifti_b0'
 )
-
-
 
 
 
@@ -173,22 +324,25 @@ dwi2response = pe.Node(
      ),
     name='dwi2response'
 )
- 
+
+
+
+
+
 tractography_wf.connect(dwiextract, 'out_file', reduce_dimension, 'in_file')
 tractography_wf.connect(reduce_dimension, 'out_file', mrconvert_mif_to_nifti_b0, 'in_file')
 
 
-# MNI to T1 
 
-tractography_wf.connect(data_source, 'T1_brain', affine_initializer, 'moving_image')
+tractography_wf.connect(template_source, 'T1_brain', affine_initializer, 'moving_image')
 tractography_wf.connect(mrconvert_mif_to_nifti_b0, 'out_file', affine_initializer, 'fixed_image')
 
-tractography_wf.connect(data_source, 'T1_brain', registration_affine, 'moving_image')
+tractography_wf.connect(template_source, 'T1_brain', registration_affine, 'moving_image')
 tractography_wf.connect(mrconvert_mif_to_nifti_b0, 'out_file', registration_affine, 'fixed_image')
 tractography_wf.connect(affine_initializer, 'out_file', registration_affine , 'initial_moving_transform')
 
 
-tractography_wf.connect(data_source, 'T1_brain', registration_nl, 'moving_image')
+tractography_wf.connect(template_source, 'T1_brain', registration_nl, 'moving_image')
 tractography_wf.connect(mrconvert_mif_to_nifti_b0, 'out_file', registration_nl, 'fixed_image')
 
 tractography_wf.connect(registration_affine, 'forward_transforms', registration_nl, 'initial_moving_transform')
@@ -197,9 +351,10 @@ tractography_wf.connect(registration_affine, 'forward_invert_flags', registratio
 tractography_wf.connect(registration_nl, 'forward_transforms', apply_registration, 'transforms')
 tractography_wf.connect(registration_nl, 'forward_invert_flags', apply_registration, 'invert_transform_flags')
 
-tractography_wf.connect(data_source, 'roi', apply_registration, 'input_image')
-tractography_wf.connect(mrconvert_mif_to_nifti_b0, 'out_file', apply_registration, 'reference_image')
 
+
+tractography_wf.connect(merge, 'merged_file', apply_registration, 'input_image')   # create the parcellations 
+tractography_wf.connect(mrconvert_mif_to_nifti_b0, 'out_file', apply_registration, 'reference_image')
 
 
 
@@ -243,7 +398,13 @@ tractography_wf.connect(mrconvert_nifti_to_mif, 'out_file', tckgen, 'seed_image'
 data_sink = pe.Node(DataSink(), name="datasink")
 data_sink.inputs.base_directory = out_dir
 
-""""
+
+"""
+tractography_wf.connect(apply_registration, 'output_image', plot_NiiGZ, 'NIIGZ_file')  
+tractography_wf.connect(mrconvert_mif_to_nifti_b0, 'out_file', plot_NiiGZ, 'background')
+tractography_wf.connect(plot_NiiGZ, 'output_image', data_sink, 'niigz_image')
+
+
 # Define the output node
 output_node = pe.Node(
     interface=util.IdentityInterface(
@@ -299,7 +460,7 @@ convert_float2int =  pe.Node(interface = Convert_float2int,
           name = 'convert_float2int')
 
 tractography_wf.connect(apply_registration, 'output_image', convert_float2int, 'float_image')  
-#tractography_wf.connect(data_source, 'roi', convert_float2int, 'float_image')  
+#tractography_wf.connect(data_source, 'parc', convert_float2int, 'float_image')  
 
 
 
@@ -340,7 +501,7 @@ def plotTDI (TDI_file, background):
     )
     stat_img = nib.load(TDI_file)
     
-    plotting.plot_stat_map(stat_img,bg_img = background , display_mode='mosaic', cut_coords= 3, title='Statistical map', output_file= output_file)
+    plotting.plot_stat_map(stat_img,bg_img = background , display_mode='mosaic', black_bg = False, cut_coords= 5, title='Statistical map', output_file= output_file)
     return output_file   
 
 PlotTDI = Function(
@@ -370,7 +531,7 @@ def plotROI (ROI_file, background):
     )
     roi_img = nib.load(ROI_file)
     
-    plotting.plot_roi(roi_img,bg_img = background , display_mode='mosaic', cut_coords= 3, title='ROI map', output_file= output_file)
+    plotting.plot_roi(roi_img,bg_img = background , display_mode='mosaic',black_bg = False, cut_coords= 5, title='ROI map', output_file= output_file)
     return output_file   
 
 PlotROI = Function(
@@ -396,12 +557,6 @@ tractography_wf.connect(plot_roi, 'output_image', data_sink, 'roi_image')
 # Run the workflow
 tractography_wf.run(plugin='MultiProc', plugin_args={'dont_resubmit_completed_jobs':True})
 
-
-# plot with the b0 image 
-# add Affine then non linear registration from MNI space to the subject's b0 image.
-# warp parcellation (from nilearn / or ROI) using ANTS and the output of the registration.
-# Conclusion : warp the parcellation from the MNI space to the subject space
-# plot the parcellation on the subject space .
 
 
 
