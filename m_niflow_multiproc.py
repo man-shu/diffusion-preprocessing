085883c358c6d11deaa8a9cb0a6cea163b0540ba
Ziad <ziad-ahmed-abdelmotagaly.mohamed@inria.fr>
2023-08-04 12:36:56 +0200

    multi_proc

--- m_niflow_multiproc.py
+++ m_niflow_multiproc.py
@@ -19,51 +19,56 @@ import sys
 import nipype.interfaces.fsl.utils as fslu
 from nipype import Merge
 from nipype.interfaces.freesurfer import ReconAll, MRIsConvert, MRIConvert
-from nipype import config, logging, Workflow
+from nipype import logging, Workflow
 import argparse
+import submitit
+import functools
 
 from niflow.nipype1.workflows.dmri.fsl.dti import bedpostx_parallel
 import diffusion_pipelines.diffusion_preprocessing as dp
-import warnings
 
 
 #warnings.filterwarnings("ignore")
 
-def create_workflow(subject_id):
+def create_workflow(id_values):
 
 
-    #config = configparser.ConfigParser()
-    #config.read(sys.argv[1])
+
+ 
+    '''config.update_config({'logging': {'log_directory': os.path.join(os.getcwd(), 'logs'),
+                                    'workflow_level': 'DEBUG',
+                                    'interface_level': 'DEBUG',
+                                    'log_to_file': True
+                                    },
+                        'execution': {'stop_on_first_crash': True},
+                    })'''
+
+
 
 
     # Define the paths to the input and output files
 
     out_dir = '/data/parietal/store/work/zmohamed/mathfun/output'
     PATH = '/data/parietal/store/work/zmohamed/mathfun/'
-    seed_num = 50000
-
-
 
 
     subject_list = config['DEFAULT']['id_list'].split(" ")
+
     visits = list(config['DEFAULT']['visits'])
     subjects_dir = config['DEFAULT']['subjects_dir']
     sessions = list(config['DEFAULT']['sessions'])
 
+    print("Key_val" + subject_list[id_values])
+   
+
     infosource = pe.Node(IdentityInterface(fields=['subject_id', 'visit','session']),
                         name='subjects')
-    #infosource.iterables = [('subject_id', subject_list), ('visit', visits), ('session', sessions)]
-    infosource.iterables = [('subject_id', subject_id), ('visit', visits), ('session', sessions)]
+    #infosource.inputs.subject_id = subject_list[id_values] 
+    #infosource.inputs.subject_id = id_values
 
+    infosource.iterables = [('subject_id', [subject_list[id_values]] ), ('visit', visits), ('session', sessions)]
+    
 
-
-    subject_id_visit = pe.Node(
-        interface=Function(
-            input_names=['subject_id', 'visit'], output_names=['composite_id'],
-            function=lambda subject_id, visit: '{}_{}'.format(subject_id, visit)
-        ),
-        name='subject_id_visit'
-    )  
 
     data_source = pe.Node(DataGrabber(infields=[],
                                     outfields=['dwi', 'bval', 'bvec', 'mask', 'roi','template', 'T1', 'T1_brain', 'parc']),
@@ -71,6 +76,7 @@ def create_workflow(subject_id):
 
     data_source.inputs.sort_filelist = True
     data_source.inputs.base_directory = config['DEFAULT']['base_directory']
+    #data_source.inputs.base_directory = '/data/parietal/store/work/zmohamed/mathfun/raw_data_visit1'
     data_source.inputs.template = ''
 
 
@@ -86,48 +92,12 @@ def create_workflow(subject_id):
         for template in data_source.inputs.field_template.keys()
     }
 
-
-    roi_source = pe.Node(DataGrabber(infields=[]),
-                            name='rois')
-    roi_source.inputs.sort_filelist = True
-    roi_source.inputs.base_directory = config['ROIS']['directory']
-    roi_source.inputs.template = 'combined_BN*bin*.nii.gz'
-
-
-    template_source = pe.Node(DataGrabber(infields=[], outfields=['T1', 'T1_brain', 'T1_mask', 'T2', 'T2_brain', 'T2_mask']),
-                            name='mni_template')
-    template_source.inputs.sort_filelist = True
-    template_source.inputs.base_directory = config['TEMPLATE']['directory']
-    template_source.inputs.template = ''
-    template_source.inputs.field_template = {
-        'T1': config['TEMPLATE']['T1'],
-        'T1_brain': config['TEMPLATE']['T1_brain'],
-        'T1_mask': config['TEMPLATE']['T1_mask'],
-        'T2': config['TEMPLATE']['T2'],
-        'T2_brain': config['TEMPLATE']['T2_brain'],
-    }
-    template_source.inputs.template_args = {
-        template: []
-        for template in template_source.inputs.field_template.keys()
-    }
-
-
     mrconvert_nifti_to_mif = pe.Node(
     interface=mrt.MRConvert( 
         out_file='dwi.mif'
         
     ),
     name='mrconvert'
-    )
-
-
-
-    dwidenoise = pe.Node(
-    interface=mrt.DWIDenoise( 
-        out_file='dwi_denoise.mif'
-        
-    ),
-    name='mrdenoise'
     )
 
 
@@ -138,6 +108,7 @@ def create_workflow(subject_id):
     ),
     name='dwiextract'
     )
+
 
     reduce_dimension = pe.Node(
     interface=mrt.MRMath(
@@ -156,6 +127,38 @@ def create_workflow(subject_id):
     ),
     name='mrconvert_mif_to_nifti_b0'
     )
+
+
+    template_source = pe.Node(DataGrabber(infields=[], outfields=['T1', 'T1_brain', 'T1_mask', 'T2', 'T2_brain', 'T2_mask']),
+                            name='mni_template')
+    template_source.inputs.sort_filelist = True
+    template_source.inputs.base_directory = config['TEMPLATE']['directory']
+    #template_source.inputs.base_directory  = '/data/parietal/store/work/zmohamed/mathfun/hcp_templates'
+    template_source.inputs.template = ''
+    
+    
+    template_source.inputs.field_template = {
+        'T1': config['TEMPLATE']['T1'],
+        'T1_brain': config['TEMPLATE']['T1_brain'],
+        'T1_mask': config['TEMPLATE']['T1_mask'],
+        'T2': config['TEMPLATE']['T2'],
+        'T2_brain': config['TEMPLATE']['T2_brain'],
+    }
+
+
+    '''template_source.inputs.field_template = {
+        'T1':  'MNI152_T1_1mm.nii.gz',
+        'T1_brain': 'MNI152_T1_1mm_brain.nii.gz',
+        'T1_mask': 'MNI152_T1_1mm_brain_mask.nii.gz',
+        'T2': 'MNI152_T2_1mm.nii.gz',
+        'T2_brain': 'MNI152_T2_1mm_brain.nii.gz',
+    }'''
+
+
+    template_source.inputs.template_args = {
+        template: []
+        for template in template_source.inputs.field_template.keys()
+    }
 
 
     affine_initializer = pe.Node(
@@ -192,8 +195,6 @@ def create_workflow(subject_id):
     registration_affine.inputs.output_warped_image = 'output_warped_image.nii.gz'
 
 
-
-
     registration_nl = pe.Node(interface=ants.Registration(), name='registration_nl')
     registration_nl.inputs.num_threads = 16
     registration_nl.inputs.metric = ['MI']
@@ -216,9 +217,6 @@ def create_workflow(subject_id):
 
 
 
-    #apply_registration = pe.MapNode(interface=ants.ApplyTransforms(),
-    #                              name='apply_registration', iterfield=['input_image'])
-
     apply_registration = pe.Node(interface=ants.ApplyTransforms(),
                                 name='apply_registration')
     apply_registration.inputs.dimension = 3
@@ -226,13 +224,17 @@ def create_workflow(subject_id):
     apply_registration.inputs.interpolation = 'NearestNeighbor'
 
 
-
-
     #bedp = pe.Node(BEDPOSTX5(n_fibres=5), name = 'bedpostx')
     params = dict(n_fibres = 2, fudge = 1, burn_in = 1000, n_jumps = 1250, sample_every = 25)
     bedp = bedpostx_parallel('nipype_bedpostx', params=params)
 
 
+    roi_source = pe.Node(DataGrabber(infields=[]),
+                            name='rois')
+    roi_source.inputs.sort_filelist = True
+    roi_source.inputs.base_directory = config['ROIS']['directory']
+    #roi_source.inputs.base_directory  = '/data/parietal/store/work/zmohamed/mathfun/dwi_rois/1mm'
+    roi_source.inputs.template = 'combined_BN*bin*.nii.gz'
 
 
 
@@ -261,8 +263,6 @@ def create_workflow(subject_id):
     tractography_wf.connect(infosource, 'visit', data_source, 'visit')
     tractography_wf.connect(infosource, 'session', data_source, 'session')
 
-    tractography_wf.connect(infosource, 'subject_id', subject_id_visit, 'subject_id')
-    tractography_wf.connect(infosource, 'visit', subject_id_visit, 'visit')
 
     tractography_wf.connect(data_source, 'dwi', mrconvert_nifti_to_mif, 'in_file')
     tractography_wf.connect(mrconvert_nifti_to_mif, 'out_file', dwiextract, 'in_file')
@@ -290,7 +290,6 @@ def create_workflow(subject_id):
 
     tractography_wf.connect(registration_nl, 'forward_transforms', apply_registration, 'transforms')
     tractography_wf.connect(registration_nl, 'forward_invert_flags', apply_registration, 'invert_transform_flags')
-
 
     tractography_wf.connect(template_source, 'T1_mask', apply_registration, 'input_image')   # create the parcellations 
     tractography_wf.connect(mrconvert_mif_to_nifti_b0, 'out_file', apply_registration, 'reference_image')
@@ -320,75 +319,48 @@ def create_workflow(subject_id):
     tractography_wf.connect(apply_registration, 'output_image', bedp, 'mask')
 
     '''
-
-    return tractography_wf
-
-
-
-   
-
-def run_workflow(value_A):
-    workflow = create_workflow(value_A)
-
     # Run the workflow
-    workflow.run(plugin='MultiProc', plugin_args={'n_procs':20, 'memory_gb': 8,'dont_resubmit_completed_jobs':True})
-
-
-'''tractography_wf.run(plugin='SLURM',
-                     plugin_args={
-                         'dont_resubmit_completed_jobs':
-				 True, 'max_jobs': 5
-                     }) '''
-                     
-
-'''tractography_wf.run(plugin='SLURMGraph',
-                     plugin_args={
-                         'dont_resubmit_completed_jobs':
-				 True,
-                         'sbatch_args':
-				 '--oversubscribe ' +
-				 '-N 1 -n 10 ' +
-				 '--time 7-0 ' +
-				 f'-e {os.path.join(os.getcwd(), "slurm_out")}/slurm_%40j.out ' +
-				 f'-o {os.path.join(os.getcwd(), "slurm_out")}/slurm_%40j.out ' +
-                                 f'-p parietal,normal ' 
-				 '--exclude=node[25-32] '
-                     }) '''
-
-
-if __name__ == "__main__":
+    tractography_wf.run(plugin='MultiProc', plugin_args={'n_procs':20, 'memory_gb': 8, "timeout": 3600, 'dont_resubmit_completed_jobs':True})
 
 
 
-    config.update_config({'logging': {'log_directory': os.path.join(os.getcwd(), 'logs'),
-                                    'workflow_level': 'DEBUG',
-                                    'interface_level': 'DEBUG',
-                                    'log_to_file': True
-                                    },
-                        'execution': {'stop_on_first_crash': True},
-                    })
-    #config.enable_debug_mode()
-
-    
-
-    config = configparser.ConfigParser()
-    config.read(sys.argv[1])
 
 
-    values = config['DEFAULT']['id_list'].split(" ")
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--id_list", type=int, help="Path to id_list")
-
-    args, _ = parser.parse_known_args()
-
-    #run_workflow(args.id_list)
+'''
+id_values = ['7014','7035']
+#visits = [1]
+#sessions =[1]
 
 
-    # Call the run_workflow function with each value from the config
-    for subject_id in values:
-        run_workflow(subject_id)
+log_folder = "log_test/%j"
+executor = submitit.AutoExecutor(folder=log_folder)
+# the following line tells the scheduler to only run\
 
- 
+#jobs = executor.map_array(create_workflow, id_values)  # just a list of jobs
+
+fns = [functools.partial(create_workflow, a) for a in id_values]
+executor.submit_array(fns)
+'''
+
+
+
+config = configparser.ConfigParser()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--id_to_process", type=int)
+parser.add_argument('--config', type = str)
+
+
+args, _ = parser.parse_known_args()
+config.read(args.config)
+
+print("Args" + str(args.id_to_process))
+
+
+# Call the run_workflow function with each value from the config
+create_workflow(args.id_to_process)
+
+
 
 
