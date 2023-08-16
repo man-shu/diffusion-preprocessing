8faee8fd9885d278123b89db8ef590bcd45d1324
Ziad <ziad-ahmed-abdelmotagaly.mohamed@inria.fr>
2023-08-16 10:15:49 +0200

    final_sh_version

--- submit.sh
+++ submit.sh
@@ -4,11 +4,15 @@
 #SBATCH --output=res_test_job_%A_%a.txt
 #
 #SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --partition=normal
-#SBATCH --mem-per-cpu=8
+#SBATCH --mem-per-cpu=320
 #SBATCH --error error_%A_%a.out
+#SBATCH --exclusive
+
 #
-#SBATCH --array=0-73
+#SBATCH --array=48-49
 
 srun python m_niflow_multiproc.py --config mg_test.cfg --id_to_process $SLURM_ARRAY_TASK_ID
 
