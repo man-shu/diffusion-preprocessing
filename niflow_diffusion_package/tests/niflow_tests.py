371ca9e989f1b59bd3dcb2ec65784395704d149d
Ziad <ziad-ahmed-abdelmotagaly.mohamed@inria.fr>
2023-09-18 07:15:06 +0000

    Update file niflow_tests.py

--- niflow_diffusion_package/tests/niflow_tests.py
+++ niflow_diffusion_package/tests/niflow_tests.py
@@ -4,13 +4,14 @@ import os
 
 class TestNiflow(unittest.TestCase):
 
+    def setUp(self):
+        self.flip = (-1, 1, 1)
+        self.bvecs_in = ('/data/parietal/store/work/zmohamed/mathfun/raw_data_visit1/dti_raw.bvecs')
+
 
     def test_bvec_flip_valid(self):
 
-        flip = (-1, 1, 1)
-        bvecs_in= ('/data/parietal/store/work/zmohamed/mathfun/raw_data_visit1/dti_raw.bvecs')
-
-        file_path = m_niflow_multiproc_lite.bvec_flip(bvecs_in, flip)
+        file_path = m_niflow_multiproc_lite.bvec_flip(self.bvecs_in, self.flip)
 
         # Check if the file path exists
         self.assertIsInstance(file_path, str)
