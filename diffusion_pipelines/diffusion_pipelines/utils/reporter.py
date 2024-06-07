3cdacf36697ed7cdaa1d0ae5780c8bc7bd6d7354
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2024-06-07 10:45:03 +0200

    insert svg content instead of file path

--- diffusion_pipelines/diffusion_pipelines/utils/reporter.py
+++ diffusion_pipelines/diffusion_pipelines/utils/reporter.py
@@ -4,6 +4,10 @@ from niworkflows.interfaces.reportlets.registration import (
 )
 from nipype.interfaces.utility.wrappers import Function
 from nipype import IdentityInterface, Node, Workflow
+import os
+
+TEMPLATE_ROOT = os.path.join(os.path.dirname(__file__), "report_template")
+REPORT_TEMPLATE = os.path.join(TEMPLATE_ROOT, "report_template.html")
 
 
 def _get_dwi_zero(dwi_file):
@@ -24,7 +28,49 @@ def _get_dwi_zero(dwi_file):
     return out_file
 
 
-def init_report_wf(name="reporter"):
+def create_html_report(
+    calling_wf_name,
+    report_wf_name,
+    plot_bet,
+    plot_before_after_eddy,
+    plot_transformed,
+    template_path,
+    output_root,
+):
+    import os
+    import string
+    from nilearn.plotting.html_document import HTMLDocument
+    import base64
+
+    def _embed_svg(to_embed, template_path=template_path):
+        with open(template_path) as f:
+            template_text = f.read()
+        string_template = string.Template(template_text)
+        string_text = string_template.safe_substitute(**to_embed)
+        f.close()
+
+        return string_text
+
+    def _get_html_text(*args):
+        to_embed = {}
+        for plot in args:
+            if plot is not None:
+                svg_path = os.path.join(plot, "report.svg")
+                with open(svg_path, "rb") as f:
+                    svg_text = str(base64.b64encode(f.read()), "utf-8")
+                to_embed[plot] = svg_text
+        return _embed_svg(to_embed)
+
+    plot_dir = [plot_bet, plot_before_after_eddy, plot_transformed]
+    html_text = _get_html_text(*plot_dir)
+    out_file = os.path.join(
+        output_root, calling_wf_name, report_wf_name, "report.html"
+    )
+    report_html = HTMLDocument(html_text).save_as_html(out_file)
+    return out_file
+
+
+def init_report_wf(calling_wf_name, output_root, name="reporter"):
     """Create a workflow to generate a report for the diffusion preprocessing
     pipeline.
 
@@ -74,7 +120,31 @@ def init_report_wf(name="reporter"):
     plot_bet = Node(SimpleShowMaskRPT(), name="plot_bet")
     # this node plots the transformed mask as an outline on transformed dwi
     # image
-    plot_mask = Node(SimpleShowMaskRPT(), name="plot_mask")
+    plot_transformed = Node(SimpleShowMaskRPT(), name="plot_transformed")
+
+    # embed plots in a html template
+    CreateHTML = Function(
+        input_names=[
+            "calling_wf_name",
+            "report_wf_name",
+            "plot_bet",
+            "plot_before_after_eddy",
+            "plot_transformed",
+            "template_path",
+            "output_root",
+        ],
+        output_names=["out_file"],
+        function=create_html_report,
+    )
+    create_html = Node(CreateHTML, name="create_html")
+    create_html.inputs.calling_wf_name = calling_wf_name
+    create_html.inputs.report_wf_name = name
+    create_html.inputs.plot_bet = plot_bet.name
+    create_html.inputs.plot_before_after_eddy = plot_before_after_eddy.name
+    create_html.inputs.plot_transformed = plot_transformed.name
+    create_html.inputs.template_path = REPORT_TEMPLATE
+    create_html.inputs.output_root = output_root
+
     workflow = Workflow(name=name)
     workflow.connect(
         [
@@ -110,13 +180,25 @@ def init_report_wf(name="reporter"):
             # plot the transformed mask as an outline on transformed dwi image
             (
                 inputnode,
-                plot_mask,
+                plot_transformed,
                 [
                     ("dwi_rigid_registered", "background_file"),
                     ("mask", "mask_file"),
                 ],
             ),
+            # create the html report
+            # (
+            #     inputnode,
+            #     create_html,
+            #     [
+            #         ("calling_wf_name", "calling_wf_name"),
+            #         (workflow.name, "report_wf_name"),
+            #         (plot_bet.name, "plot_bet"),
+            #         (plot_before_after_eddy.name, "plot_before_after_eddy"),
+            #         (plot_transformed.name, "plot_transformed"),
+            #     ],
+            # ),
         ]
     )
-
+    workflow.add_nodes([create_html])
     return workflow
