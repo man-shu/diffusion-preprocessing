701ab444d2a673579c604324b1dcbfa1ef8ddfda
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2024-10-14 12:47:29 +0200

    more figures in reporting

--- diffusion_pipelines/diffusion_pipelines/utils/reporter.py
+++ diffusion_pipelines/diffusion_pipelines/utils/reporter.py
@@ -62,10 +62,9 @@ def create_html_report(
         return _embed_svg(to_embed)
 
     html_text = _get_html_text(*plots)
-    out_file = os.path.join(
-        output_root, calling_wf_name, report_wf_name, "report.html"
-    )
+    out_file = os.path.join(output_root, "report.html")
     report_html = HTMLDocument(html_text).save_as_html(out_file)
+    print(f"Report for {calling_wf_name} created at {out_file}")
     return out_file
 
 
@@ -100,6 +99,8 @@ def init_report_wf(calling_wf_name, output_root, name="reporter"):
                 "mask",
                 "bet_mask",
                 "dwi_rigid_registered",
+                "template_t2_initial",
+                "template_t2_masked",
             ]
         ),
         name="reporter_inputnode",
@@ -112,6 +113,9 @@ def init_report_wf(calling_wf_name, output_root, name="reporter"):
     get_intial_zero = Node(DWIZero, name="get_intial_zero")
     # this node is used to get the zero index of the eddy corrected dwi file
     get_eddy_zero = get_intial_zero.clone("get_eddy_zero")
+    # this node is used to get the zero index of the t2 template registered dwi file
+    get_registered_zero = get_intial_zero.clone("get_registered_zero")
+
     # this node plots the before and after images of the eddy correction
     plot_before_after_eddy = Node(
         SimpleBeforeAfter(), name="plot_before_after_eddy"
@@ -119,6 +123,21 @@ def init_report_wf(calling_wf_name, output_root, name="reporter"):
     # set labels for the before and after images
     plot_before_after_eddy.inputs.before_label = "Distorted"
     plot_before_after_eddy.inputs.after_label = "Eddy Corrected"
+    # this node plots before and after images of masking T2 template
+    plot_before_after_mask_t2 = Node(
+        SimpleBeforeAfter(), name="plot_before_after_mask_t2"
+    )
+    # set labels for the before and after images
+    plot_before_after_mask_t2.inputs.before_label = "T2 Template"
+    plot_before_after_mask_t2.inputs.after_label = "Masked T2 Template"
+    # this node plots the masked T2 template as before and the dwi registeresd
+    # to it as after
+    plot_before_after_t2_dwi = Node(
+        SimpleBeforeAfter(), name="plot_before_after_t2_dwi"
+    )
+    # set labels for the before and after images
+    plot_before_after_t2_dwi.inputs.before_label = "Masked T2 Template"
+    plot_before_after_t2_dwi.inputs.after_label = "Registered DWI"
     # this node plots the extracted brain mask as outline on the initial dwi
     # image
     plot_bet = Node(SimpleShowMaskRPT(), name="plot_bet")
@@ -126,8 +145,9 @@ def init_report_wf(calling_wf_name, output_root, name="reporter"):
     # image
     plot_transformed = Node(SimpleShowMaskRPT(), name="plot_transformed")
 
-    # Create a Merge node to combine the outputs of plot_bet, plot_before_after_eddy, and plot_transformed
-    merge_node = Node(Merge(3), name="merge_node")
+    # Create a Merge node to combine the outputs of plot_bet,
+    # plot_before_after_eddy, and plot_transformed
+    merge_node = Node(Merge(5), name="merge_node")
 
     # embed plots in a html template
     CreateHTML = Function(
@@ -154,6 +174,12 @@ def init_report_wf(calling_wf_name, output_root, name="reporter"):
             (inputnode, get_intial_zero, [("dwi_initial", "dwi_file")]),
             # get the zero index of the eddy corrected dwi file
             (inputnode, get_eddy_zero, [("eddy_corrected", "dwi_file")]),
+            # get the zero index of the dwi file registered to the t2 template
+            (
+                inputnode,
+                get_registered_zero,
+                [("dwi_rigid_registered", "dwi_file")],
+            ),
             # plot the extracted brain mask as outline on the initial dwi image
             (
                 inputnode,
@@ -179,6 +205,36 @@ def init_report_wf(calling_wf_name, output_root, name="reporter"):
                     ("out", "after"),
                 ],
             ),
+            # plot the initial T2 template as before
+            (
+                inputnode,
+                plot_before_after_mask_t2,
+                [
+                    ("template_t2_initial", "before"),
+                ],
+            ),
+            # plot the masked T2 template as after
+            (
+                inputnode,
+                plot_before_after_mask_t2,
+                [
+                    ("template_t2_masked", "after"),
+                ],
+            ),
+            # plot the masked T2 template as before and transformed dwi as
+            # after
+            (
+                inputnode,
+                plot_before_after_t2_dwi,
+                [
+                    ("template_t2_masked", "before"),
+                ],
+            ),
+            (
+                get_registered_zero,
+                plot_before_after_t2_dwi,
+                [("out", "after")],
+            ),
             # plot the transformed mask as an outline on transformed dwi image
             (
                 inputnode,
@@ -188,7 +244,8 @@ def init_report_wf(calling_wf_name, output_root, name="reporter"):
                     ("mask", "mask_file"),
                 ],
             ),
-            # merge the outputs of plot_bet, plot_before_after_eddy, and plot_transformed
+            # merge the outputs of plot_bet, plot_before_after_eddy,
+            # plot_before_after_mask_t2, plot_transformed
             (
                 plot_bet,
                 merge_node,
@@ -204,10 +261,22 @@ def init_report_wf(calling_wf_name, output_root, name="reporter"):
                 ],
             ),
             (
-                plot_transformed,
+                plot_before_after_mask_t2,
                 merge_node,
                 [
                     ("out_report", "in3"),
+                ],
+            ),
+            (
+                plot_before_after_t2_dwi,
+                merge_node,
+                [("out_report", "in4")],
+            ),
+            (
+                plot_transformed,
+                merge_node,
+                [
+                    ("out_report", "in5"),
                 ],
             ),
             # create the html report
