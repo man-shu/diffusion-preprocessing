0f9d33218735e1f4013d9f81df12ea81f9480d19
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-01-17 14:13:21 +0100

    add init in workflows

---
+++ diffusion_pipelines/diffusion_pipelines/workflows/__init__.py
@@ -0,0 +1,5 @@
+from .preprocess import init_preprocess_wf
+from .recon import init_recon_wf
+from .report import init_report_wf
+
+__all__ = ["init_preprocess_wf", "init_recon_wf", "init_report_wf"]
