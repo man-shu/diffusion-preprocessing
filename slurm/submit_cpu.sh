5e2d444e4da797228d21f0d63d920990d91d13e7
Himanshu Aggarwal <haggarwa@margaret.saclay.inria.fr>
2025-03-20 12:36:48 +0100

    update paths

--- slurm/submit_cpu.sh
+++ slurm/submit_cpu.sh
@@ -5,6 +5,6 @@
 #SBATCH --partition=parietal,normal
 #SBATCH --error error_%A_%a.out
 
-srun python runners/run_tracto_drago.py
+srun python /data/parietal/store3/work/haggarwa/diffusion/diffusion_preprocessing/runners/run_tracto_drago.py
 
 
---
+++ slurm/submit_gpu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#SBATCH --job-name=bedpostx_gpu
+#SBATCH --partition=gpu
+#SBATCH --error error_%A_%a.out
+#SBATCH --gres=gpu:1
+
+srun python /data/parietal/store3/work/haggarwa/diffusion/diffusion_preprocessing/runners/run_tracto_drago.py
