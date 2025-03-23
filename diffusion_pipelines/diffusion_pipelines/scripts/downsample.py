f2b0eb905e4e151b4df1cd848ec783f4f21b5038
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-03-23 13:05:23 +0100

    downsample WAND data, subject name in the title on reports

--- diffusion_pipelines/diffusion_pipelines/scripts/downsample.py
+++ diffusion_pipelines/diffusion_pipelines/scripts/downsample.py
@@ -6,13 +6,13 @@ import os
 import numpy as np
 
 # Define the input and output file paths
-root_path = "/Users/himanshu/Desktop/diffusion/bids_data/sub-7014/"
+root_path = "/Users/himanshu/Desktop/diffusion/data/WAND-bids/"
 
 # get all nifti files
 nifti_files = glob(f"{root_path}**/*.nii*", recursive=True)
 
 # Define the output directory
-output_dir = "/Users/himanshu/Desktop/diffusion/downsampled_data"
+output_dir = "/Users/himanshu/Desktop/diffusion/data/WAND-downsampled/"
 os.makedirs(output_dir, exist_ok=True)
 
 # Define the target resolution as 5mm isotropic
@@ -20,8 +20,14 @@ target_resolution = (5, 5, 5)
 # %%
 # Downsample all nifti files
 for nii in nifti_files:
-    img = resample_img(nii, target_affine=np.diag(target_resolution))
-    output_path = os.path.join(output_dir, os.path.basename(nii))
+    try:
+        img = resample_img(nii, target_affine=np.diag(target_resolution))
+    except Exception as e:
+        print(f"Error: {e}")
+        continue
+    _output_dir = os.path.dirname(nii.replace(root_path, output_dir))
+    os.makedirs(_output_dir, exist_ok=True)
+    output_path = os.path.join(_output_dir, os.path.basename(nii))
     img.to_filename(output_path)
 
 # %%
