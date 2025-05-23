d922f5e15a24d86441fa18ab501ecf54bc6d6073
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-05-23 16:51:22 +0200

    full paths

--- .deprecated/slurm/submit_wand_concat.sh
+++ .deprecated/slurm/submit_wand_concat.sh
@@ -5,4 +5,4 @@
 #SBATCH --partition=parietal,normal
 #SBATCH --error error_%A_%a.out
 
-srun singularity exec --env-file singularity_env.txt --bind ./data:/home/input diffusion_pipelines.sif /opt/miniconda3/bin/diffusion_pipelines -< config-concat.cfg
+srun singularity exec --env-file /data/parietal/store3/work/haggarwa/diffusion/diffusion-preprocessing/singularity_env.txt --bind /data/parietal/store3/work/haggarwa/diffusion/diffusion-preprocessing/data:/home/input /data/parietal/store3/work/haggarwa/diffusion/diffusion-preprocessing/diffusion_pipelines.sif /opt/miniconda3/bin/diffusion_pipelines -< /data/parietal/store3/work/haggarwa/diffusion/diffusion-preprocessing/config-concat.cfg
