a0dd2008d030f2aaac5b9be53482e94387bdd5f3
Himanshu <himanshu.aggarwal@inria.fr>
2025-06-13 13:37:29 +0200

    lesser parallel jobs

--- .deprecated/slurm/submit_wand_concat.sh
+++ .deprecated/slurm/submit_wand_concat.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #
 #SBATCH --job-name=wand_concat_preproc
-#SBATCH -c100
+#SBATCH -c20
 #SBATCH --partition=parietal,normal
 #SBATCH --error error_%A_%a.out
+#SBATCH --mem=0
+
+module load singularity
 
 srun singularity exec --env-file /data/parietal/store3/work/haggarwa/diffusion/diffusion-preprocessing/singularity_env.txt --bind /data/parietal/store3/work/haggarwa/diffusion/diffusion-preprocessing/data:/home/input /data/parietal/store3/work/haggarwa/diffusion/diffusion-preprocessing/diffusion_pipelines.sif /opt/miniconda3/bin/diffusion_pipelines -< /data/parietal/store3/work/haggarwa/diffusion/diffusion-preprocessing/config-concat.cfg
