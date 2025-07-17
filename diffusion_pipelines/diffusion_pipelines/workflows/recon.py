08889575cc01b0b121e46d7f14ed9f405b08bf5f
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-07-17 15:34:05 +0200

    fix

--- diffusion_pipelines/diffusion_pipelines/workflows/recon.py
+++ diffusion_pipelines/diffusion_pipelines/workflows/recon.py
@@ -5,7 +5,7 @@ from bids.layout import BIDSLayout
 
 
 def init_recon_wf(output_dir, config):
-    output_spaces = config.output_spaces.checkpoint()
+    config.output_spaces.checkpoint()
 
     # Initialize BIDSLayout
     layout = BIDSLayout(root=config.bids_dir, validate=True)
@@ -31,7 +31,7 @@ def init_recon_wf(output_dir, config):
         skull_strip_mode=config.skull_strip_mode,
         skull_strip_fixed_seed=config.skull_strip_fixed_seed,
         skull_strip_template=config.skull_strip_template[0],
-        spaces=output_spaces,
+        spaces=config.output_spaces,
         bids_filters=None,
         cifti_output=config.cifti_output,
     )
