8155b72e306ca5d794da4ccbdf1eb2e15b43ee32
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-07-31 18:37:21 +0200

    fix?

--- diffusion_pipelines/diffusion_pipelines/workflows/preproc.py
+++ diffusion_pipelines/diffusion_pipelines/workflows/preproc.py
@@ -349,14 +349,14 @@ def _preprocess_wf(name="preprocess", bet_frac=0.34, output_dir="."):
             (
                 bbreg_wf,
                 rotate_gradients,
-                [("outputnode.out_lta_file", "lta_file")],
+                [("out_lta_file", "lta_file")],
             ),
             # apply the registration to the skull-stripped and eddy-corrected
             # dwi using FreeSurfer's ApplyVolTransform
             (
                 bbreg_wf,
                 apply_registration,
-                [("outputnode.out_lta_file", "lta_file")],
+                [("out_lta_file", "lta_file")],
             ),
             (
                 eddycorrect,
@@ -372,7 +372,7 @@ def _preprocess_wf(name="preprocess", bet_frac=0.34, output_dir="."):
             (
                 bbreg_wf,
                 apply_registration_mask,
-                [("outputnode.out_lta_file", "lta_file")],
+                [("out_lta_file", "lta_file")],
             ),
             (bet, apply_registration_mask, [("mask_file", "source_file")]),
             (
@@ -403,7 +403,7 @@ def _preprocess_wf(name="preprocess", bet_frac=0.34, output_dir="."):
             (
                 bbreg_wf,
                 output,
-                [("outputnode.out_lta_file", "rigid_dwi_2_t1")],
+                [("out_lta_file", "rigid_dwi_2_t1")],
             ),
             (input_subject, output, [("preprocessed_t1", "t1_initial")]),
             (strip_t1, output, [("out_file", "t1_masked")]),
