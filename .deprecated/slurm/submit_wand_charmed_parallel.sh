7a0b4f715a85829e729f6d6ed49c11c7c2a8c99e
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-08-29 10:51:51 +0200

    fix mounting in singularity command

--- .deprecated/slurm/submit_wand_charmed_parallel.sh
+++ .deprecated/slurm/submit_wand_charmed_parallel.sh
@@ -15,12 +15,12 @@ module load singularity
 
 srun singularity exec \
 --env-file /data/parietal/store3/work/haggarwa/diffusion/diffusion-preprocessing/singularity_env.txt \
---bind /data/parietal/store3/work/haggarwa/diffusion/diffusion-preprocessing/data,/data/parietal/store4/data:/home/input \
+--bind /data/parietal/store3/work/haggarwa/diffusion/diffusion-preprocessing/data:/home/output,/data/parietal/store4/data:/home/input \
 /data/parietal/store3/work/haggarwa/diffusion/diffusion-preprocessing/diffusion-preprocessing_main_singularity.sif \
 /opt/miniconda3/bin/diffusion_pipelines \
 /home/input/WAND \
-/home/input/WAND-concat/derivatives \
---work-dir /home/input/cache \
+/home/output/WAND-concat/derivatives \
+--work-dir /home/output/cache \
 --output-spaces fsLR:den-32k MNI152NLin6Asym T1w fsaverage5 \
 --cifti-output 91k \
 --nprocs 1 \
@@ -29,7 +29,7 @@ srun singularity exec \
 --acquisition CHARMED \
 --phase-encoding-direction AP \
 --preproc \
---preproc-t1 /home/input/WAND-concat/derivatives/smriprep/sub-${dirs[${SLURM_ARRAY_TASK_ID}]:36}/ses-02/anat/sub-${dirs[${SLURM_ARRAY_TASK_ID}]:36}_ses-02_desc-preproc_T1w.nii.gz \
---preproc-t1-mask /home/input/WAND-concat/derivatives/smriprep/sub-${dirs[${SLURM_ARRAY_TASK_ID}]:36}/ses-02/anat/sub-${dirs[${SLURM_ARRAY_TASK_ID}]:36}_ses-02_desc-brain_mask.nii.gz \
---fs-native-to-t1w-xfm /home/input/WAND-concat/derivatives/smriprep/sub-${dirs[${SLURM_ARRAY_TASK_ID}]:36}/ses-02/anat/sub-${dirs[${SLURM_ARRAY_TASK_ID}]:36}_ses-02_from-fsnative_to-T1w_mode-image_xfm.txt \
+--preproc-t1 /home/output/WAND-concat/derivatives/smriprep/sub-${dirs[${SLURM_ARRAY_TASK_ID}]:36}/ses-02/anat/sub-${dirs[${SLURM_ARRAY_TASK_ID}]:36}_ses-02_desc-preproc_T1w.nii.gz \
+--preproc-t1-mask /home/output/WAND-concat/derivatives/smriprep/sub-${dirs[${SLURM_ARRAY_TASK_ID}]:36}/ses-02/anat/sub-${dirs[${SLURM_ARRAY_TASK_ID}]:36}_ses-02_desc-brain_mask.nii.gz \
+--fs-native-to-t1w-xfm /home/output/WAND-concat/derivatives/smriprep/sub-${dirs[${SLURM_ARRAY_TASK_ID}]:36}/ses-02/anat/sub-${dirs[${SLURM_ARRAY_TASK_ID}]:36}_ses-02_from-fsnative_to-T1w_mode-image_xfm.txt \
 --debug
