c10bc25bb0d74691bfe20e159460aa7629bb8b5c
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-09-22 17:07:18 +0200

    debug

--- diffusion_pipelines/diffusion_pipelines/workflows/bids.py
+++ diffusion_pipelines/diffusion_pipelines/workflows/bids.py
@@ -63,6 +63,7 @@ def collect_data(
     if isinstance(bids_dir, BIDSLayout):
         layout = bids_dir
     else:
+        print(f"Initializing BIDSLayout with root: {bids_dir}")
         layout = BIDSLayout(
             bids_dir=bids_dir,
             validate=False,
@@ -144,7 +145,7 @@ def init_bidsdata_wf(config, name="bidsdata_wf"):
     )
 
     subject_data, layout = collect_data(
-        str(config.bids_dir),
+        config.bids_dir,
         config.participant_label,
         session_id=config.session_label,
         bids_filters=bids_filters,
