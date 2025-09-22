b23ad2b495055c5d493a328a6ca53469ad706f7d
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-09-22 14:53:01 +0200

    create bids source node from identity interface

--- diffusion_pipelines/diffusion_pipelines/workflows/bids.py
+++ diffusion_pipelines/diffusion_pipelines/workflows/bids.py
@@ -3,26 +3,36 @@ from bids.utils import listify
 from nipype import Node, Workflow, IdentityInterface
 from nipype.interfaces.utility import Function
 from nipype.interfaces.io import SelectFiles
-from niworkflows.interfaces.bids import BIDSFreeSurferDir
+from niworkflows.interfaces.bids import BIDSDataGrabber
 import os
 from pathlib import Path
 
 DEFAULT_BIDS_QUERIES = {
     "dwi": {
         "datatype": "dwi",
-        "extension": [".nii", ".nii.gz", ".bval", ".bvec"],
+        "extension": [".nii", ".nii.gz"],
+    },
+    "bval": {
+        "datatype": "dwi",
+        "extension": [".bval"],
+    },
+    "bvec": {
+        "datatype": "dwi",
+        "extension": [".bvec"],
     },
     "t1w": {
         "datatype": "anat",
         "suffix": "T1w",
         "desc": "preproc",
         "space": None,
+        "extension": [".nii", ".nii.gz"],
     },
     "brain_mask": {
         "datatype": "anat",
         "suffix": "mask",
         "desc": "brain",
         "space": None,
+        "extension": [".nii", ".nii.gz"],
     },
     "fsnative2t1w_xfm": {
         "datatype": "anat",
@@ -30,8 +40,13 @@ DEFAULT_BIDS_QUERIES = {
         "to": "T1w",
         "extension": [".txt"],
     },
-    "figures": {
+    "plot_recon_surface_on_t1": {
         "extension": [".svg"],
+        "suffix": "T1w",
+    },
+    "plot_recon_segmentations_on_t1": {
+        "extension": [".svg"],
+        "suffix": "dseg",
     },
 }
 
@@ -53,7 +68,6 @@ def collect_data(
     session_id = session_id or Query.OPTIONAL
     layout_get_kwargs = {
         "return_type": "file",
-        "extension": [".nii", ".nii.gz"],
         "subject": participant_label,
         "session": session_id,
     }
@@ -79,21 +93,9 @@ def collect_data(
 
         queries[acq].update(entities)
 
-    for acq, entities in queries.items():
-        for entity in list(layout_get_kwargs.keys()):
-            if acq == "figures" and entity == "extension":
-                continue
-            if entity in entities:
-                queries[acq][entity] = listify(
-                    layout_get_kwargs[entity]
-                ) + listify(entities[entity])
-                queries[acq][entity] = set(queries[acq][entity])
-                queries[acq][entity] = list(queries[acq][entity])
-            else:
-                queries[acq][entity] = layout_get_kwargs[entity]
-
     subj_data = {
-        dtype: sorted(layout.get(**query)) for dtype, query in queries.items()
+        dtype: sorted(layout.get(**layout_get_kwargs, **query))
+        for dtype, query in queries.items()
     }
 
     # Filter out unwanted files
@@ -102,7 +104,7 @@ def collect_data(
     for dtype, files in subj_data.items():
         selected = []
         for f in files:
-            if dtype == "dwi":
+            if dtype in ["dwi", "bval", "bvec"]:
                 if "derivative" not in f:
                     selected.append(f)
             else:
@@ -111,7 +113,7 @@ def collect_data(
         subj_data[dtype] = selected
 
         if (
-            dtype != "dwi"
+            dtype not in ["dwi", "bval", "bvec"]
             and len(subj_data[dtype]) == 0
             and not config.recon
             and config.preproc
@@ -145,33 +147,10 @@ def init_bidsdata_wf(config, name="bidsdata_wf"):
         bids_validate=False,
     )
 
-    # Create SelectFiles node
-    sf = Node(
-        SelectFiles(
-            templates, base_directory=str(config.bids_dir), sort_filelist=True
-        ),
-        name="selectfiles",
+    bids_datasource = Node(
+        IdentityInterface(fields=subject_data.keys()), name="bids_datasource"
     )
-
-    sf.inputs.acquisition = config.acquisition
-    sf.inputs.phase_encoding_direction = config.phase_encoding_direction
-    layout = BIDSLayout(str(config.bids_dir))
-    # set subjects as iterables
-    # if subject is not specified, all subjects will be processed
-    if config.participant_label == ["all"]:
-        sf.iterables = [("subject_id", layout.get_subjects())]
-    # otherwise, only the specified subjects will be processed
-    elif isinstance(config.participant_label, list):
-        for subject in config.participant_label:
-            if subject not in layout.get_subjects():
-                raise ValueError(f"Subject {subject} not found in dataset")
-        sf.iterables = [("subject_id", config.participant_label)]
-
-    if config.session_label is not None:
-        sf.iterables.append(("session_id", config.session_label))
-    # all sessions
-    else:
-        sf.iterables.append(("session_id", layout.get_sessions()))
+    bids_datasource.inputs.trait_set(**subject_data)
 
     ### Node to decode entities
     def decode_entities(file_name):
@@ -187,28 +166,6 @@ def init_bidsdata_wf(config, name="bidsdata_wf"):
     )
 
     decode_entities = Node(DecodeEntities, name="decode_entities")
-
-    ### Node to only pick one file
-    def pick_files(file_name_list):
-        from bids.layout import parse_file_entities
-
-        if isinstance(file_name_list, str):
-            return file_name_list
-
-        # Filter files based on space
-        for file_name in file_name_list:
-            entities = parse_file_entities(file_name)
-            if entities["space"] is None:
-                return file_name
-
-    PickFiles = Function(
-        input_names=["file_name_list"],
-        output_names=["file_name"],
-        function=pick_files,
-    )
-    pick_t1 = Node(PickFiles, name="pick_t1")
-    pick_mask = pick_t1.clone("pick_mask")
-    pick_xfm = pick_t1.clone("pick_xfm")
 
     output = Node(
         IdentityInterface(
@@ -229,20 +186,17 @@ def init_bidsdata_wf(config, name="bidsdata_wf"):
     bidsdata_wf = Workflow(name=name)
     bidsdata_wf.connect(
         [
-            (sf, decode_entities, [("dwi", "file_name")]),
-            (sf, pick_t1, [("preprocessed_t1", "file_name_list")]),
-            (sf, pick_mask, [("preprocessed_t1_mask", "file_name_list")]),
-            (sf, pick_xfm, [("fsnative2t1w_xfm", "file_name_list")]),
-            (sf, output, [("dwi", "dwi")]),
-            (sf, output, [("bval", "bval")]),
-            (sf, output, [("bvec", "bvec")]),
+            (bids_datasource, decode_entities, [("dwi", "file_name")]),
+            (bids_datasource, output, [("dwi", "dwi")]),
+            (bids_datasource, output, [("bval", "bval")]),
+            (bids_datasource, output, [("bvec", "bvec")]),
             (
-                sf,
+                bids_datasource,
                 output,
                 [("plot_recon_surface_on_t1", "plot_recon_surface_on_t1")],
             ),
             (
-                sf,
+                bids_datasource,
                 output,
                 [
                     (
@@ -251,9 +205,17 @@ def init_bidsdata_wf(config, name="bidsdata_wf"):
                     )
                 ],
             ),
-            (pick_t1, output, [("file_name", "preprocessed_t1")]),
-            (pick_mask, output, [("file_name", "preprocessed_t1_mask")]),
-            (pick_xfm, output, [("file_name", "fsnative2t1w_xfm")]),
+            (bids_datasource, output, [("t1w", "preprocessed_t1")]),
+            (
+                bids_datasource,
+                output,
+                [("brain_mask", "preprocessed_t1_mask")],
+            ),
+            (
+                bids_datasource,
+                output,
+                [("fsnative2t1w_xfm", "fsnative2t1w_xfm")],
+            ),
         ]
     )
 
