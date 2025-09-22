3b86b0db21d4963055743eb98407b5cb21db616b
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-09-22 16:38:20 +0200

    import json

--- diffusion_pipelines/diffusion_pipelines/workflows/bids.py
+++ diffusion_pipelines/diffusion_pipelines/workflows/bids.py
@@ -6,6 +6,7 @@ from nipype.interfaces.io import SelectFiles
 from niworkflows.interfaces.bids import BIDSDataGrabber
 import os
 from pathlib import Path
+import json
 
 DEFAULT_BIDS_QUERIES = {
     "dwi": {
