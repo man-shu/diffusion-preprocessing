7219f44e93496385aeb29525e44c8eb504d06dff
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-09-30 10:17:36 +0200

    pull the function out of wf initiation

--- diffusion_pipelines/diffusion_pipelines/workflows/sink.py
+++ diffusion_pipelines/diffusion_pipelines/workflows/sink.py
@@ -3,70 +3,72 @@ from nipype.interfaces.utility import Function
 from nipype.interfaces.io import DataSink
 
 
+### build the full file name
+def build_substitutions(bids_entities):
+
+    import os
+    from pathlib import Path
+
+    def _build_bids(bids_entities):
+        replacements = {
+            "subject": "sub-",
+            "session": "_ses-",
+            "acquisition": "_acq-",
+            "direction": "_dir-",
+            "part": "_part-",
+        }
+        bids_name = ""
+        for key, value in bids_entities.items():
+            if key in replacements:
+                bids_name += f"{replacements[key]}{value}"
+        return bids_name
+
+    bids_name = _build_bids(bids_entities)
+
+    substitutions = [
+        (
+            "initial_mean_bzero_brain_mask_warped",
+            f"{bids_name}_space-individualT1_desc-mask+bbreg_dwi",
+        ),
+        (
+            "vol0000_flirt_merged_warped",
+            f"{bids_name}_space-individualT1_desc-eddycorrected+bbreg_dwi",
+        ),
+        ("vol0000_flirt_merged", f"{bids_name}_desc-eddycorrected_dwi"),
+        (
+            f"registered_mean_bzero",
+            f"{bids_name}_space-individualT1_"
+            "desc-eddycorrected+bbreg+meanb0_dwi",
+        ),
+        (
+            f"{bids_name}_dwi_rot.bvec",
+            f"{bids_name}_desc-rotated_dwi.bvec",
+        ),
+    ]
+
+    breakpoint()
+
+    # add root directory in substitutions
+    for i, (src, dst) in enumerate(substitutions):
+        if bids_entities["session"]:
+            prefix = os.path.join(
+                "sub-" + bids_entities["subject"],
+                "ses-" + bids_entities["session"],
+                "dwi",
+            )
+        else:
+            prefix = os.path.join("sub-" + bids_entities["subject"], "dwi")
+
+        substitutions[i] = (src, os.path.join(prefix, dst))
+
+    return substitutions
+
+
 def init_sink_wf(config, name="sink_wf"):
 
     inputnode = Node(
         IdentityInterface(fields=["bids_entities"]), name="sinkinputnode"
     )
-
-    ### build the full file name
-    def build_substitutions(bids_entities):
-
-        import os
-        from pathlib import Path
-
-        def _build_bids(bids_entities):
-            replacements = {
-                "subject": "sub-",
-                "session": "_ses-",
-                "acquisition": "_acq-",
-                "direction": "_dir-",
-                "part": "_part-",
-            }
-            bids_name = ""
-            for key, value in bids_entities.items():
-                if key in replacements:
-                    bids_name += f"{replacements[key]}{value}"
-            return bids_name
-
-        bids_name = _build_bids(bids_entities)
-
-        substitutions = [
-            (
-                "initial_mean_bzero_brain_mask_warped",
-                f"{bids_name}_space-individualT1_desc-mask+bbreg_dwi",
-            ),
-            (
-                "vol0000_flirt_merged_warped",
-                f"{bids_name}_space-individualT1_desc-eddycorrected+bbreg_dwi",
-            ),
-            ("vol0000_flirt_merged", f"{bids_name}_desc-eddycorrected_dwi"),
-            (
-                f"registered_mean_bzero",
-                f"{bids_name}_space-individualT1_"
-                "desc-eddycorrected+bbreg+meanb0_dwi",
-            ),
-            (
-                f"{bids_name}_dwi_rot.bvec",
-                f"{bids_name}_desc-rotated_dwi.bvec",
-            ),
-        ]
-
-        # add root directory in substitutions
-        for i, (src, dst) in enumerate(substitutions):
-            breakpoint()
-            if bids_entities["session"]:
-                prefix = os.path.join(
-                    "sub-" + bids_entities["subject"],
-                    "ses-" + bids_entities["session"],
-                    "dwi",
-                )
-            else:
-                prefix = os.path.join("sub-" + bids_entities["subject"], "dwi")
-
-            substitutions[i] = (src, os.path.join(prefix, dst))
-
-        return substitutions
 
     BuildSubstitutions = Function(
         input_names=["bids_entities"],
