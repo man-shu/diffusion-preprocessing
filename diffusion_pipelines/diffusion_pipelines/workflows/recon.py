2e8e1f71523f98faee1f042848be14deceb2be9c
Himanshu <himanshuaggarwal1997@gmail.com>
2026-04-20 17:12:11 +0200

    do independent session wise t1w preprocessing

--- diffusion_pipelines/diffusion_pipelines/workflows/recon.py
+++ diffusion_pipelines/diffusion_pipelines/workflows/recon.py
@@ -4,6 +4,47 @@ from smriprep.workflows.base import init_smriprep_wf
 from bids.layout import BIDSLayout
 
 
+def _subject_session_list(config):
+    # First check that bids_dir looks like a BIDS folder
+    bids_dir = config.bids_dir.resolve()
+    layout = BIDSLayout(str(bids_dir), validate=False)
+    subject_list = collect_participants(
+        layout, participant_label=config.participant_label
+    )
+    session_list = config.session_label or []
+
+    subject_session_list = []
+    for subject in subject_list:
+        sessions = (
+            layout.get_sessions(
+                scope="raw",
+                subject=subject,
+                session=session_list or Query.OPTIONAL,
+                suffix=[
+                    "T1w",
+                    "T2w",
+                ],
+            )
+            or None
+        )
+
+        if config.subject_anatomical_reference == "sessionwise":
+            if not sessions:
+                raise RuntimeError(
+                    '--subject-anatomical-reference "sessionwise" was,'
+                    " requested, but no sessions "
+                    f"found for subject {subject}."
+                )
+            for session in sessions:
+                subject_session_list.append((subject, session))
+        else:
+            # This will use all sessions either found by layout or passed in
+            # via --session-id
+            subject_session_list.append((subject, sessions))
+
+    return subject_session_list
+
+
 def init_recon_wf(output_dir, config):
     config.output_spaces.checkpoint()
 
@@ -34,5 +75,6 @@ def init_recon_wf(output_dir, config):
         spaces=config.output_spaces,
         bids_filters=None,
         cifti_output=config.cifti_output,
+        subject_session_list=_subject_session_list(config),
     )
     return wf
